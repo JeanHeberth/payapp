4202b83ad2f834d53a67706bb1919a50a1f0b00b
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2023-10-20 15:23:39 -0300

    Alterando o slf4j

--- src/test/java/br/com/payapp/RealizarLoginTest.java
+++ src/test/java/br/com/payapp/RealizarLoginTest.java
@@ -3,6 +3,8 @@ package br.com.payapp;
 import br.com.payapp.core.DriveFactory;
 import br.com.payapp.page.LoginPage;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +12,8 @@ import static org.testng.Assert.*;
 
 
 public class RealizarLoginTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(RealizarLoginTest.class);
 
     private DriveFactory driveFactory = new DriveFactory();
 
@@ -21,6 +25,7 @@ public class RealizarLoginTest {
     void beforeMethod(){
         driver = driveFactory.setUp();
         loginPage = new LoginPage(driver);
+        logger.info("Executando testes!");
     }
 
     @AfterMethod
@@ -34,7 +39,7 @@ public class RealizarLoginTest {
         assertTrue(loginPage.validarURL());
         loginPage.clicarNumeroCinco();
     }
-    @Test(invocationCount = 10)
+    @Test(invocationCount = 1)
     public void B_realizarLoginComDadosInvalidos(){
         loginPage.realizarLogin("222222222");
         assertEquals("CPF NÃO CADASTRADO", loginPage.validarCpfNaoEncontrado());
